c1cbd042f5c283ee2fe81c137f56336e15f5d813
engrajibulhasan <engrajibulhasan@gmail.com>
2022-07-27 11:17:54 +0600

    Setup still going on

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv=require("dotenv");
 const mongoose=require("mongoose");
+const path=require("path");
+const cookieParser = require("cookie-parser");
 
 const app=express();
 dotenv.config();
 
+//DB connection
 mongoose.connect(process.env.DB_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true
@@ -12,5 +15,18 @@ mongoose.connect(process.env.DB_URI,{
 .then((res)=>console.log('Connected with mongo DB'))
 .catch((err)=>console.log(err))
 
+// request parser
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+//Parse cookies - Signed cookie
+app.use(cookieParser(process.env.COOKIE_SECRET))
+
+//Use static folder
+app.use(express.static(path.join(__dirname,"public")))
+
+//  Set view Engine
+app.set("view engine","ejs")
+
+
+
